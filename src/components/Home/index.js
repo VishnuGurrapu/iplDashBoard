a357aeb964b897fd1a1f1b98d6c9c7d5a45b017e
VishnuGurrapu <gurrapuvishnu20@gmail.com>
2024-08-25 07:32:09 +0000

    second commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,16 @@
 // Write your code here
 import {Component} from 'react'
 
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
 import './index.css'
 
 import TeamCard from '../TeamCard'
 
 class Home extends Component {
-  state = {teamsData: []}
+  state = {teamsData: [], isLoading: true}
 
   componentDidMount() {
     this.getTeamsData()
@@ -21,25 +25,34 @@ class Home extends Component {
       teamImageUrl: eachItem.team_image_url,
     }))
     console.log(formattedData)
-    this.setState({teamsData: formattedData})
+    this.setState({teamsData: formattedData, isLoading: false})
   }
+
   render() {
-    const {teamsData} = this.state
+    const {teamsData, isLoading} = this.state
     return (
       <div className="home-container">
-        <div className="home-heading-container">
-          <img
-            className="home-logo"
-            src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
-            alt="ipl logo"
-          />
-          <h1 className="home-heading">IPL Dashboard</h1>
-        </div>
-        <ul className="teams-list">
-          {teamsData.map(item => (
-            <TeamCard teamData={item} key={item.id} />
-          ))}
-        </ul>
+        {isLoading ? (
+          <div data-testid="loader">
+            <Loader type="Oval" color="#ffffff" height={50} width={50} />
+          </div>
+        ) : (
+          <>
+            <div className="home-heading-container">
+              <img
+                className="home-logo"
+                src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+                alt="ipl logo"
+              />
+              <h1 className="home-heading">IPL Dashboard</h1>
+            </div>
+            <ul className="teams-list">
+              {teamsData.map(item => (
+                <TeamCard teamData={item} key={item.id} />
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     )
   }
--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -10,13 +10,15 @@ const MatchCard = props => {
     matchStatus,
     result,
   } = teamData
-
+  const bgColor = matchStatus === 'Won' ? 'bgGreen' : 'bgRed'
   return (
     <div className="matchCard-container">
-      <img className="comLogo" src={competingTeamLogo} />
+      <div className="comLogoContainer">
+        <img className="comLogo" src={competingTeamLogo} />
+      </div>
       <p className="para12">{competingTeam}</p>
       <p className="para32">{result}</p>
-      <p className="para22">{matchStatus}</p>
+      <p className={`para22 ${bgColor}`}>{matchStatus}</p>
     </div>
   )
 }
